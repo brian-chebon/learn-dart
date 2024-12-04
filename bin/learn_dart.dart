2ab6106a3bbfebd4f96308d5acfee48024a4ba02
Brian Chebon <brianlchebon@gmail.com>
2024-12-04 20:50:13 +0300

    chore: added the cal logic

--- bin/learn_dart.dart
+++ bin/learn_dart.dart
@@ -1,5 +1,8 @@
 import 'package:learn_dart/learn_dart.dart' as learn_dart;
 
 void main(List<String> arguments) {
-  print('Hello world: ${learn_dart.calculate()}!');
+  print(learn_dart.calculateAdd());
+  print(learn_dart.calculateSubtract());
+  print(learn_dart.calculateMultiply());
+  print(learn_dart.calculateDivide());
 }
